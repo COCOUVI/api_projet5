a80bca48a671455d88ba7ff3fb19b411696e273d
Alexandro <cocouvialexandro74@gmail.com>
2025-04-02 13:28:18 +0100

    modify Requestregister

--- app/Http/Requests/RegisterUser.php
+++ app/Http/Requests/RegisterUser.php
@@ -5,52 +5,47 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
 
 class RegisterUser extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
-            
-            'name'=>'required',
-            'email'=>'required|unique:users,email',
-            'password'=>'required'
-
-
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
         ];
     }
-    public function failedValidation(Validator $validator){
 
+    public function failedValidation(Validator $validator)
+    {
         throw new HttpResponseException(response()->json([
             'success' => false,
-            'status' => 422,
+            'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
             'error' => true,
             'message' => 'Erreur de validation',
-            'errorList' => $validator->errors()->toArray() // Convertir les erreurs en tableau
-        ])->header('Content-Type', 'application/json'));
+            'errorList' => $validator->errors()->toArray(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY)->header('Content-Type', 'application/json'));
     }
 
-    public function messages(){
+    public function messages(): array
+    {
         return [
-               'name.required'=>'le nom est requis',
-               'email.required'=>'Une adresse email est requis',
-                'email.unique'=>'cette adresse email existe deja',
-                'password.required'=>'le mot de passe est requis'
+            'name.required' => 'Le nom est requis.',
+            'name.string' => 'Le nom doit être une chaîne de caractères.',
+            'name.max' => 'Le nom ne doit pas dépasser 255 caractères.',
+            'email.required' => 'Une adresse e-mail est requise.',
+            'email.email' => 'L\'adresse e-mail doit être valide.',
+            'email.unique' => 'Cette adresse e-mail est déjà utilisée.',
+            'password.required' => 'Le mot de passe est requis.',
+            'password.string' => 'Le mot de passe doit être une chaîne de caractères.',
+            'password.min' => 'Le mot de passe doit contenir au moins 8 caractères.',
         ];
-    
-    } 
+    }
 }
