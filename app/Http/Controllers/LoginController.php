6de70dc57ce609b3beee116070ca6adb714b9ebd
Alexandro <cocouvialexandro74@gmail.com>
2025-04-05 08:58:53 +0100

    modify Connecting

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ecole;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -11,13 +14,18 @@ class LoginController extends Controller
     public function Connecting(Request $request){
         if( Auth::attempt($request->only(['email','password']))){
             $user=Auth::user();
+            $ecole = DB::table('ecoles')
+                        ->where('id_user', '=', $user->id)
+                        ->first();
             $token=$user->createToken('key_secret_only_for_backend')->plainTextToken;
+            // dd($ecole);
             return response()->json([
                 "status"=>403,
                 "message"=>"User is login",
                 "username"=>$user->name,
                 "statut"=>$user->statut,
-                "token"=>$token
+                "token"=>$token,
+                "type"=> $ecole ? $ecole->type:""
             ]);
         } else{
 
